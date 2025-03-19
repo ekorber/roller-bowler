fa14045e1d32d89b02f1f490b2441087db2cf783
Eric Korber <erickorber1994@gmail.com>
2025-03-19 14:24:39 +0900

    Fix and improve player controller

--- Features/Player/player.gd
+++ Features/Player/player.gd
@@ -2,25 +2,43 @@ extends RigidBody3D
 class_name Player
 
 @export var level_geometry: Node3D
+@export var level_pivot: Node3D
 @export var rotation_sensitivity: float = 0.5
+@onready var ground_raycast: RayCast3D = $RayCast3D
 @onready var camera: Camera3D = get_viewport().get_camera_3d()
-@onready var original_pos = level_geometry.global_position
+@onready var original_player_pos = position
 var can_control_player: bool = true
 var rot_x: float = 0
 var rot_y: float = 0
+var push_x: float = 0
+var push_z: float = 0
+var max_push: float = 20
 
-func _physics_process(_delta: float) -> void:
-	# Get the camera's orientation
+func _physics_process(delta: float) -> void:
+	# Rotate level geometry
 	var camera_basis = camera.global_transform.basis
-	level_geometry.transform.basis = Basis()
-	rotate_around_point(camera_basis.z, rot_x * rotation_sensitivity * 0.001)
-	rotate_around_point(camera_basis.x, rot_y * rotation_sensitivity * 0.001)
+	level_pivot.position = position
+	level_geometry.position = original_player_pos - position
+	level_pivot.transform.basis = Basis()
+	level_pivot.rotate(camera_basis.z, rot_x * rotation_sensitivity * 0.001)
+	level_pivot.rotate(camera_basis.x, -rot_y * rotation_sensitivity * 0.001)
+	
+	# Apply additional force to ball
+	push_x = clampf((rot_x * delta), -max_push, max_push)
+	push_z = clampf((rot_y * delta), -max_push, max_push)
+	if (push_x > 0 and linear_velocity.x < 0) or (push_x < 0 and linear_velocity.x > 0):
+		push_x *= 2.0
+	if (push_z > 0 and linear_velocity.z < 0) or (push_z < 0 and linear_velocity.z > 0):
+		push_z *= 2.0
+	apply_central_force(Vector3(push_x, 0, push_z))
+
+	# Prevent ball from slipping through geometry
+	ground_raycast.global_position = global_position
+	if ground_raycast.is_colliding():
+		global_position = ground_raycast.get_collision_point() + Vector3(0, 0.5, 0)
+
 
 func _input(event: InputEvent) -> void:
 	if can_control_player and event is InputEventMouseMotion:
-		rot_x += event.relative.x
-		rot_y += event.relative.y
-
-func rotate_around_point(axis, angle):
-	level_geometry.global_position = global_position + (original_pos - global_position).rotated(axis, angle)
-	level_geometry.rotate(axis, angle)
+		rot_x = clampf(rot_x + event.relative.x, -100, 100)
+		rot_y = clampf(rot_y + event.relative.y, -100, 100)
